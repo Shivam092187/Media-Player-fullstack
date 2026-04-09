5fb3b6dfacc5f7570d43b0ae5ea2e4c094e883df
shivamkumar <shivamkumar212017@gmail.com>
2026-04-09 12:13:20 +0530

    Fix loginInput, update login API & Login.jsx

--- Frontend/spotify-music-player/src/services/api.js
+++ Frontend/spotify-music-player/src/services/api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-//  Backend URL from .env (local or live)
 const API = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_UR || "https://media-player-fullstack-1.onrender.com/api/auth",
-  withCredentials: true, // 🔥 cookie support for cross-domain
+  baseURL: import.meta.env.REACT_APP_API_URL, // Backend URL from env
 });
 
-// 🔥 Attach token automatically if exists
+//  TOKEN AUTO ATTACH
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -15,13 +13,10 @@ API.interceptors.request.use((config) => {
   return config;
 });
 
-//  Auth APIs
-export const registerUser = (data) => API.post("/register", data);
-export const loginUser = (data) => API.post("/login", data);
-export const getMe = () => API.get("/me");
-
-// Music APIs
-export const getAllSongs = () => API.get("/music");
-export const playSong = (id) => API.get(`/music/play/${id}`);
+// APIs
+export const getAllSongs = () => API.get("/api/music");
+export const playSong = (id) => API.get(`/api/music/play/${id}`);
+export const registerUser = (data) => API.post("/api/auth/register", data);
+export const loginUser = (data) => API.post("/api/auth/login", data);
 
 export default API;
