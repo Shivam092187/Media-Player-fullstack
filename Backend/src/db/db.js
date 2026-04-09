e55124638cb24352c99aa9d6127a69de29d55dd8
shivamkumar <shivamkumar212017@gmail.com>
2026-04-09 20:53:08 +0530

    Fixed MONGO_URI typo and DB connection

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connectDB(req,res) {
     try {
-        await mongoose.connect(process.env.MONGODB_URl)
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log("Database connected successfully");
     } catch (error) {
         console.log("Database not connected successfully", error);
