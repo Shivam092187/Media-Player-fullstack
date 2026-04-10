638afc657995bb8e4a9e63c87b8226a12df7f037
shivamkumar <shivamkumar212017@gmail.com>
2026-04-10 09:39:13 +0530

    fix: api exports issue fixed

--- Frontend/spotify-music-player/src/services/api.js
+++ Frontend/spotify-music-player/src/services/api.js
@@ -8,14 +8,27 @@ const API = axios.create({
 // token attach
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
-export const registerUser = (data) =>
-  API.post("/api/auth/register", data);
 
-export const loginUser = (data) =>
-  API.post("/api/auth/login", data);
+export const getAllSongs = () => {
+  return API.get("/api/music");
+};
+
+export const playSong = (id) => {
+  return API.get(`/api/music/play/${id}`);
+};
+
+export const registerUser = (data) => {
+  return API.post("/api/auth/register", data);
+};
+
+export const loginUser = (data) => {
+  return API.post("/api/auth/login", data);
+};
 
 export default API;
